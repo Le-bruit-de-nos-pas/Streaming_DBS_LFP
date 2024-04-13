86fc016b298cc0f65cfac58dba8651f0f3a087e4
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2024-04-13 17:59:44 +0200

    Add files via upload

--- perceive_raw_tf.m
+++ perceive_raw_tf.m
@@ -1,8 +1,10 @@
 function  [tf, t, f] = perceive_raw_tf(data, fs, timewindow, timestep)
+
     % Calculate the number of FFT points based on timewindow and sampling frequency
     nfft = round(timewindow / fs * 1000);
     %  Convert timestep from seconds to milliseconds
-    timestep = round(timestep / fs * 1000);
+    timestep = round(timestep / fs * 1000); 
+
     % Compute the spectrogram of the input data
     % uses a Hanning window of length nfft (window length for FFT) to window the data before computing the FFT
     % window is shifted by nfft - timestep samples between consecutive segments
@@ -13,4 +15,5 @@ function  [tf, t, f] = perceive_raw_tf(data, fs, timewindow, timestep)
     % t contains the time instants
     % P is a matrix containing the power spectral density
     [~, f, t, tf] = spectrogram(data, hann(nfft), nfft-timestep, nfft, fs, 'yaxis', 'power');
+    
 end
