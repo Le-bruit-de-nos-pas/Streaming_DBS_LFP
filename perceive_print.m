54207156467e4d614eaba824dbae0872eb61a085
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2024-04-13 18:01:04 +0200

    Add files via upload

--- perceive_print.m
+++ perceive_print.m
@@ -6,15 +6,19 @@ function perceive_print(filename)
     if ~exist(fold, 'dir')
         mkdir(fold);
     end
-
+  
     % print figure to a PNG file with specific settings
     print(gcf, fullfile(fold, file), '-dpng', '-r300', '-opengl');
     % save the current orientation to reset it to default later
+
     or = get(gcf, 'PaperOrientation');
     % Set landscape orientation for pdf print
+
     set(gcf, 'PaperOrientation', 'Landscape');
     % Print figure to pdf with specific settings to make figure visible
+
     print(gcf, fullfile(fold, file), '-dpdf', '-r80', '-bestfit');
+    
     % restore the original orientation
     set(gcf, 'PaperOrientation', or);
 end
